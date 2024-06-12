a9e15a77747afdc9cfb72c64170926d4a52bac47
Shree Jaybhay <shreejaybhay26@gmail.com>
2024-06-12 20:04:43 +0530

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="shreejaybhay26@gmail.com">
+        <a href="mailto:shreejaybhay26@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
